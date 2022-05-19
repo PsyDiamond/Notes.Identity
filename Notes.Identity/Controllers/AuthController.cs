ce3a04a3aa4125b329ee7ad306ca5f80ba29d3f7
Lex Talionis <lextalionis1488@gmail.com>
2022-05-19 11:39:05 +0100

    Add Login logic into controller

--- Notes.Identity/Controllers/AuthController.cs
+++ Notes.Identity/Controllers/AuthController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Notes.Identity.Models;
 using System;
 using System.Collections.Generic;
@@ -9,6 +11,19 @@ namespace Notes.Identity.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IIdentityServerInteractionService _identityServerInteractionService;
+
+        public AuthController(SignInManager<AppUser> signInManager, 
+            UserManager<AppUser> userManager, 
+            IIdentityServerInteractionService identityServerInteractionService)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _identityServerInteractionService = identityServerInteractionService;
+        }
+
         [HttpGet]
         public IActionResult Login(string returnUrl)
         {
@@ -21,8 +36,28 @@ namespace Notes.Identity.Controllers
         }
 
         [HttpPost]
-        public IActionResult Login(LoginViewModel viewModel)
+        public async Task<IActionResult> Login(LoginViewModel viewModel)
         {
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+
+            var user = await _userManager.FindByNameAsync(viewModel.UserName);
+            if (user == null)
+            {
+                ModelState.AddModelError(string.Empty, "User not found");
+                return View(viewModel);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(viewModel.UserName,
+                viewModel.Password, false, false);
+            if (result.Succeeded)
+            {
+                return Redirect(viewModel.ReturnUrl);
+            }
+
+            ModelState.AddModelError(string.Empty, "Loging error");
             return View(viewModel);
         }
     }
